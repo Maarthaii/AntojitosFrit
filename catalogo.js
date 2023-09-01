e19ed02eebde1212eece9bee9bdfd942af5e15a3
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-09-01 00:48:38 -0400

    Nueva manera de ocultar y mostrar carrito Solo quito y pongo una clase en el carrito Cambios en el catalago.html y catalago.js

--- catalogo.js
+++ catalogo.js
@@ -1,5 +1,19 @@
-//Variable que mantiene el estado visible del carrito
-var carritoVisible = false;
+
+//Funcion que hace visible el carrito
+function hacerVisibleCarrito(){
+    document.getElementById('carrito').classList.add('show')
+
+}
+
+//Funciòn que controla si hay elementos en el carrito. Si no hay oculto el carrito.
+function ocultarCarrito(){
+    if(carritoItems.childElementCount==0){
+        document.getElementById('carrito').classList.remove('show')
+    }
+}
+
+
+
 
 //Cargar los documentos
 if(document.readyState == 'loading'){
@@ -66,16 +80,6 @@ function agregarAlCarritoClicked(event){
     hacerVisibleCarrito();
 }
 
-//Funcion que hace visible el carrito
-function hacerVisibleCarrito(){
-    carritoVisible = true;
-    var carrito = document.getElementsByClassName('carrito')[0];
-    carrito.style.marginRight = '0';
-    carrito.style.opacity = '1';
-
-    var items =document.getElementsByClassName('contenedor-items')[0];
-    items.style.width = '60%';
-}
 
 //Funciòn que agrega un item al carrito
 function agregarItemAlCarrito(titulo, precio, imagenSrc){
@@ -160,19 +164,6 @@ function eliminarItemCarrito(event){
     //Si no hay eliminar el carrito
     ocultarCarrito();
 }
-//Funciòn que controla si hay elementos en el carrito. Si no hay oculto el carrito.
-function ocultarCarrito(){
-    var carritoItems = document.getElementsByClassName('carrito-items')[0];
-    if(carritoItems.childElementCount==0){
-        var carrito = document.getElementsByClassName('carrito')[0];
-        carrito.style.marginRight = '-100%';
-        carrito.style.opacity = '0';
-        carritoVisible = false;
-    
-        var items =document.getElementsByClassName('contenedor-items')[0];
-        items.style.width = '100%';
-    }
-}
 //Actualizar el total de Carrito
 function actualizarTotalCarrito(){
     //seleccionar el contenedor carrito
@@ -193,5 +184,5 @@ function actualizarTotalCarrito(){
     total = Math.round(total * 100)/100;
 
     document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
-
+    
 }
